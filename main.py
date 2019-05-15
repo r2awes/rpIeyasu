1731f53a31cf747e2a632aca2e01354a42169d50
Richard M <r2awes@gmail.com>
2019-05-15 10:55:06 -0400

    obd additions

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from threading import Thread
+
+from obd_capture import OBD_Capture
+from obd_sensors import SENSORS
+from obd_sensors import *
+
+from render import Render
+
+def obd_connect(o):
+	o.connect()
+
+class OBDConnection(object):
+	def __init__(self):
+		self.c = OBD_Capture()
+		def get_capture(self):
+			return self.c
+
+		def connect(self):
+			self.t = Thread(target=obd_connect, args=(self.c,))
+			self.t.start()
+
+		def is_connected(self):
+			return self.c.is_connected()
+
+		def get_output(self):
+			if self.c and self.c.is_connected():
+				return self.c.capture_data()
+			return ""
+
+		def get_port(self):
+			return self.c.is_connected()
+
+		def get_port_name(self):
+			if self.c:
+				port = self.c.is_connected()
+				if port:
+					try:
+						return port.port.name
+					except:
+						pass
+				return None
+
+		def get_sensors(self):
+			sensors = []
+			if self.c:
+				sensors = self.c.getSupportedSensorList()
+			return sensors
---
+++ render.py
@@ -0,0 +1,91 @@
+import time
+
+"""
+Uncomment to use I2C OLEDs that work with Adafruit's SSD1306 package
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+"""
+
+class Render:
+	RST = None
+	#bus_number = 1
+	conn = False
+	debug = False
+
+	def __init__(self, bus_number, title, text):
+		self.debug = True
+		self.conn = False
+		self.title = title
+		self.text = text
+		
+		"""
+		self.disp = Adafruit_SSD1306.SSD1306_128_32(rst=Render.RST, i2c_bus=bus_number)
+		try:
+			self.disp.begin()
+		except IOError: 
+			print ("Check if the ports to see if the display on " + bus_number + " is connected properly")
+			conn = False
+		"""
+		self.handle_debug(self.debug, self.conn)
+		
+	def handle_debug(self, debug, conn):
+		if( debug ):
+			self.log_text()
+
+		if( conn ):
+			self.print_text()
+
+	def log_text(self):
+		print self.text + " " + self.title
+
+	def print_text(self):
+		"""
+		self.disp.clear()
+		self.disp.display()
+
+		width = self.disp.width
+		height = self.disp.height
+		image = Image.new('1', (width, height))
+		draw = ImageDraw.Draw(image)
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		padding = -2
+		top = padding
+		#bottom = height-padding
+
+		x = 0
+
+		regular = ImageFont.truetype(font="BarlowCondensed-Regular.ttf", size=35, index=0 )
+		light = ImageFont.truetype(font="BarlowCondensed-Light.ttf", size=15, index=0 )
+		
+		try:
+			draw.rectangle((0, 0, width, height), outline=0, fill=0)
+			draw.text((x, top), self.text, font=regular, fill=255 )
+			draw.text((x+100, top+15), self.title, font=light, fill=255 )
+
+			self.disp.image(image)
+			self.disp.display()
+			time.sleep(.1)
+		finally: 
+			draw.rectangle((0, 0, width, height), outline=0, fill=0)
+		"""
+	def update_text(self, text, title):
+		self.text = text
+		if( title != "" ):
+			self.title = title
+		if( text == "clear" ):
+			self.title = ""
+			self.text = ""
+
+		if(self.debug):
+			self.log_text()
+		if(self.conn):
+			self.print_text()
+
+	def clear(self):
+		self.update_text("clear", "")
+		
