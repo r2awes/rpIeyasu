d530cb68678ac3b6418f25b047d205b8c190904e
r2awes <r2awes@gmail.com>
2019-05-30 17:50:37 -0400

    more fixes

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 from render import Render
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
 GPIO.setup( 21, GPIO.OUT, initial=GPIO.HIGH )
 
-a = Render(1)
+a = Render(4)
 b = Render(3)
-c = Render(4)
+c = Render(1)
 
-a.update_text("A", "a")
+av = GPIO.input(21)
+
+a.update_text( str(av), "a")
 b.update_text("B", "b")
 c.update_text("C", "c")
-
-print GPIO.getmode()
