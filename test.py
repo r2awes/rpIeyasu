e6b45cd38b56d358bbf878c616ca10d40c0bf471
r2awes <r2awes@gmail.com>
2019-05-30 18:02:37 -0400

    clearing improvements

--- test.py
+++ test.py
@@ -9,7 +9,12 @@ a = Render(4)
 b = Render(3)
 c = Render(1)
 
-
-a.update_text("Display ", "4")
-b.update_text("Display ", "3")
-c.update_text("Display ", "1")
+try:
+	while True: 
+		a.update_text("Display ", "4")
+		b.update_text("Display ", "3")
+		c.update_text("Display ", "1")
+except KeyboardInterrupt:
+		a.clear()
+		b.clear()
+		c.clear()
