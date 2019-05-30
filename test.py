ba6d554ce193b548a94143fb2f0e6135c6afa163
r2awes <r2awes@gmail.com>
2019-05-30 17:55:51 -0400

    better labeling

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ b = Render(3)
 c = Render(1)
 
 
-a.update_text( str(av), "a")
-b.update_text("B", "b")
-c.update_text("C", "c")
+a.update_text("Display ", "4")
+b.update_text("Display ", "3")
+c.update_text("Display ", "1")
